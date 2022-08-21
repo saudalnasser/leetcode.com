4669a0c17411d983dfc2abfe0825d606ff6e317a
saud alnasser <saud4services@gmail.com>
2022-08-22 02:56:53 +0300

    solve fizz buzz (412)

---
+++ src/fizz-buzz.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/fizz-buzz/
+
+export function fizzBuzz(n: number): string[] {
+  const list: string[] = [];
+
+  for (let i = 1; i <= n; i++)
+    if (i % 3 === 0 && i % 5 === 0) list.push('FizzBuzz');
+    else if (i % 3 === 0) list.push('Fizz');
+    else if (i % 5 === 0) list.push('Buzz');
+    else list.push(i.toString());
+
+  return list;
+}
